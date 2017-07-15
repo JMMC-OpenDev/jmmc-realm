dc85427d148597e5f51156ab75a9ecacd7c7650e
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2017-07-15 19:34:32 +0000

    update for existdb 3.3

--- src/main/java/org/exist/security/realm/jmmc/JMMCRealm.java
+++ src/main/java/org/exist/security/realm/jmmc/JMMCRealm.java
@@ -11,14 +11,20 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import javax.naming.NamingException;
 import javax.net.ssl.HttpsURLConnection;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import org.apache.log4j.Logger;
 import org.exist.EXistException;
 import org.exist.config.Configuration;
 import org.exist.config.ConfigurationException;
+import org.exist.config.Configurator;
 import org.exist.config.annotation.ConfigurationClass;
 import org.exist.config.annotation.ConfigurationFieldAsAttribute;
+import org.exist.config.annotation.ConfigurationFieldAsElement;
+import org.exist.security.AXSchemaType;
 import org.exist.security.AbstractAccount;
 import org.exist.security.AbstractRealm;
 import org.exist.security.Account;
@@ -26,15 +32,11 @@ import org.exist.security.AuthenticationException;
 import org.exist.security.Group;
 import org.exist.security.PermissionDeniedException;
 import org.exist.security.Subject;
-import org.exist.security.internal.SubjectAccreditedImpl;
-import org.exist.storage.DBBroker;
-import org.apache.log4j.Logger;
-import org.exist.config.Configurator;
-import org.exist.config.annotation.ConfigurationFieldAsElement;
-import org.exist.security.AXSchemaType;
 import org.exist.security.internal.SecurityManagerImpl;
+import org.exist.security.internal.SubjectAccreditedImpl;
 import org.exist.security.internal.aider.GroupAider;
 import org.exist.security.internal.aider.UserAider;
+import org.exist.storage.DBBroker;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
@@ -52,7 +54,7 @@ import org.w3c.dom.Node;
  * To use it:
  * <ul>
  *   <li>
- *     place the JAR file in the '<b>lib/extension</b>' directory of eXist-db as 
+ *     place the JAR file in the '<b>lib/extension</b>' directory of eXist-db as
  *     '<b>exist-security-jmmc.jar</b>'
  *   </li>
  *   <li>
@@ -72,44 +74,44 @@ import org.w3c.dom.Node;
  */
 @ConfigurationClass("realm")
 public class JMMCRealm extends AbstractRealm {
-    
-    private final static Logger LOG = Logger.getLogger(JMMCRealm.class);
-    
+
+    private final static Logger logger = Logger.getLogger(JMMCRealm.class);
+
     @ConfigurationFieldAsAttribute("id")
     public static String ID = "JMMC";
-    
+
     @ConfigurationFieldAsElement("url")
     protected String url = null;
-    
+
     public JMMCRealm(SecurityManagerImpl sm, Configuration config) {
         super(sm, config);
-        
+
         configuration = Configurator.configure(this, config);
-        
-        LOG.info("JMMCRealm created with url = '" + url + "'");
+
+        logger.info("JMMCRealm created with url = '" + url + "'");
     }
-    
+
     @Override
     public String getId() {
         return ID;
     }
-    
+
     @Override
     public Subject authenticate(String accountName, Object credentials) throws AuthenticationException {
         final boolean checked;
-        
+
         try {
             checked = checkPassword(accountName, (String) credentials);
         } catch (Exception ex) {
-            LOG.debug(ex.getMessage(), ex);
-            LOG.error(new AuthenticationException(AuthenticationException.UNNOWN_EXCEPTION, ex.getMessage()));
+            logger.debug(ex.getMessage(), ex);
+            logger.error(new AuthenticationException(AuthenticationException.UNNOWN_EXCEPTION, ex.getMessage()));
             return null;
         }
-        
+
         if (!checked) {
             throw new AuthenticationException(AuthenticationException.WRONG_PASSWORD, "Bad password for user '" + accountName + "'");
         }
-        
+
         final AbstractAccount account = (AbstractAccount) getAccount(accountName);
         if (account != null) {
             return new SubjectAccreditedImpl(account, true);
@@ -125,7 +127,7 @@ public class JMMCRealm extends AbstractRealm {
      * @param password the user password
      *
      * @return true if user exists and password is correct, false otherwise
-     * 
+     *
      * @throws Exception if the request failed
      */
     private boolean checkPassword(final String user, final String password) throws Exception {
@@ -133,9 +135,9 @@ public class JMMCRealm extends AbstractRealm {
         params.put("action", "checkPassword");
         params.put("email", user);
         params.put("password", password);
-        
+
         InputStream is = httpPost(params);
-        
+
         Document doc = parseXML(is);
         return doc.getElementsByTagName("true").item(0) != null;
     }
@@ -146,13 +148,13 @@ public class JMMCRealm extends AbstractRealm {
     class User {
 
         String email;
-        
+
         String fullName;
-        
+
         String affiliation;
-        
+
         final List<String> credentials = new ArrayList<>();
-        
+
         public User(String email, Node data) {
             this.email = email;
 
@@ -161,7 +163,7 @@ public class JMMCRealm extends AbstractRealm {
                 if (child.getNodeType() != Node.ELEMENT_NODE) {
                     continue;
                 }
-                
+
                 switch (child.getNodeName()) {
                     case "name":
                         this.fullName = child.getTextContent();
@@ -175,7 +177,7 @@ public class JMMCRealm extends AbstractRealm {
                     default:
                 }
             }
-            
+
         }
     }
 
@@ -185,22 +187,22 @@ public class JMMCRealm extends AbstractRealm {
      * @param user the JMMC user name
      *
      * @return a user description or null if user is unknown
-     * 
+     *
      * @throws Exception if the request failed
      */
     private User getInfo(final String user) throws Exception {
         Map<String, String> params = new HashMap<>();
         params.put("action", "getInfo");
         params.put("email", user);
-        
+
         InputStream is = httpPost(params);
-        
+
         Document doc = parseXML(is);
         Node response = doc.getElementsByTagName("response").item(0);
         if (response == null) {
             return null;
         }
-        
+
         return new User(user, response);
     }
 
@@ -214,30 +216,30 @@ public class JMMCRealm extends AbstractRealm {
      * @param stream the input stream
      *
      * @return the response to the request
-     * 
+     *
      * @throws Exception if the request failed
      */
     private InputStream httpPost(Map<String, String> params) throws Exception {
         HttpsURLConnection connection = (HttpsURLConnection) new URL(this.url).openConnection();
-        
+
         connection.setRequestMethod("POST");
         connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
         connection.setUseCaches(false);
         connection.setDoInput(true);
         connection.setDoOutput(true);
-        
+
         String urlParameters = "";
         for (Map.Entry<String, String> entry : params.entrySet()) {
             urlParameters += entry.getKey() + "=" + URLEncoder.encode(entry.getValue(), "UTF-8") + "&";
         }
-        
+
         connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
-        
+
         try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
             wr.writeBytes(urlParameters);
             wr.flush();
         }
-        
+
         return connection.getInputStream();
     }
 
@@ -247,16 +249,16 @@ public class JMMCRealm extends AbstractRealm {
      * @param stream the input stream
      *
      * @return the XML document
-     * 
+     *
      * @throws Exception if IO or parse error occur
      */
     private static Document parseXML(InputStream stream) throws Exception {
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        
+
         return documentBuilder.parse(stream);
     }
-    
+
     @Override
     public final synchronized Account getAccount(final String name) {
         // try to get the cached account first
@@ -268,12 +270,12 @@ public class JMMCRealm extends AbstractRealm {
                 final User user = getInfo(name);
                 return createAccount(user);
             } catch (Exception ex) {
-                LOG.error(ex.getMessage(), ex);
+                logger.error(ex.getMessage(), ex);
                 return null;
             }
         }
     }
-    
+
     @Override
     public final Group getGroup(String name) {
         Group group = super.getGroup(name);
@@ -285,7 +287,7 @@ public class JMMCRealm extends AbstractRealm {
         try {
             return createGroup(name);
         } catch (AuthenticationException ex) {
-            LOG.error(ex.getMessage(), ex);
+            logger.error(ex.getMessage(), ex);
             return null;
         }
     }
@@ -300,29 +302,29 @@ public class JMMCRealm extends AbstractRealm {
      * @throws Exception if it failed to create the account
      */
     private Account createAccount(final User user) throws Exception {
-        LOG.debug("creating account " + user.email);
+        logger.info("creating account " + user.email);
         return executeAsSystemUser(new Unit<Account>() {
             @Override
             public Account execute(DBBroker broker) throws EXistException, PermissionDeniedException {
                 final UserAider userAider = new UserAider(ID, user.email, getSecurityManager().getGroup("guest"));
                 // all user are added to the jmmc group
                 userAider.addGroup(getSecurityManager().getGroup("jmmc"));
-                
+
                 for (String credential : user.credentials) {
                     userAider.addGroup(getSecurityManager().getGroup(credential));
                 }
-                
+
                 if (user.fullName != null) {
                     userAider.setMetadataValue(AXSchemaType.FULLNAME, user.fullName);
                 }
-                
+
                 if (user.affiliation != null) {
                     // not queryable from the security manager as regular metadata item
                     userAider.setMetadataValue(JMMCSchemaType.AFFILIATION, user.affiliation);
                 }
-                
+
                 final Account account = getSecurityManager().addAccount(userAider);
-                
+
                 return account;
             }
         });
@@ -338,21 +340,35 @@ public class JMMCRealm extends AbstractRealm {
      * @throws AuthenticationException if it failed to create the group
      */
     private Group createGroup(final String groupname) throws AuthenticationException {
-        LOG.debug("creating group " + groupname);
+        logger.info("creating group " + groupname);
         try {
-            return getSecurityManager().addGroup(new GroupAider(ID, groupname));
+            final DBBroker broker = getDatabase().get(Optional.of(getSecurityManager().getSystemSubject()));
+            return getSecurityManager().addGroup(broker, new GroupAider(ID, groupname));
         } catch (PermissionDeniedException | EXistException e) {
+            logger.error(e.getMessage(), e);
             throw new AuthenticationException(AuthenticationException.UNNOWN_EXCEPTION, e.getMessage(), e);
         }
     }
-    
+
     @Override
     public boolean deleteAccount(Account account) throws PermissionDeniedException, EXistException, ConfigurationException {
         return false;
     }
-    
+
     @Override
     public boolean deleteGroup(Group group) throws PermissionDeniedException, EXistException, ConfigurationException {
         return false;
     }
+
+    private static interface Unit<R> {
+
+        public R execute(DBBroker broker) throws EXistException, PermissionDeniedException, NamingException;
+    }
+
+    private <R> R executeAsSystemUser(final Unit<R> unit) throws EXistException, PermissionDeniedException, NamingException {
+        final DBBroker broker = getDatabase().get(Optional.of(getSecurityManager().getSystemSubject()));
+        //perform as SYSTEM user
+        return unit.execute(broker);
+    }
+
 }
